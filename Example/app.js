878198bdce53ae8793acc0f734bc880223b1ef02
Snow <Snow@SnowdeMac-mini.local>
2017-09-11 11:56:59 +0800

    ios

--- Example/app.js
+++ Example/app.js
@@ -5,19 +5,52 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text ,StyleSheet,View,TouchableHighlight} from 'react-native';
 import RNKitSensor from "rnkit_sensor";
 
 export default class App extends Component {
-  
+    constructor(props){
+        super(props);
+        RNKitSensor.initial('appkey',20,5);
+        RNKitSensor.check();
+        RNKitSensor.save("11111","222222",9);
+    }
+
+    _nextPage() {
+
+    }
   render() {
-      //RNKitSensor.initial('appkey',20);
-      //RNKitSensor.check();
-      //RNKitSensor.save("11111","222222",9);
 
     return (
-        <Text>'666666666677'</Text>
+
+        <View style={styles.constainer}>
+
+            <TouchableHighlight
+                style={styles.touch}
+                onPress={this._nextPage.bind(this)}>
+                <View>
+                    <Text>下一个页面</Text>
+                </View>
+            </TouchableHighlight>
+        </View>
+
     )
 
    }
 }
+
+
+const styles = StyleSheet.create({
+    constainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    touch: {
+        marginTop: 20,
+        width: 100,
+        height: 40,
+        backgroundColor: 'green'
+    },
+
+});
--- ios/RNKitSensor.m
+++ ios/RNKitSensor.m
@@ -7,7 +7,6 @@
 //
 
 #import "RNKitSensor.h"
-#import <CommonCrypto/CommonDigest.h>
 
 #if __has_include(<React/RCTBridge.h>)
 #import <React/RCTConvert.h>
@@ -17,25 +16,11 @@
 #import "RCTLog.h"
 #endif
 
-
-#import "DataBaseHandle.h"
-#import "DataBaseModel.h"
-#import "Reachability.h"
 #import "Utils.h"
-
-#define kIOS9 ([[[UIDevice currentDevice] systemVersion] doubleValue] >= 9.0)
-
-
-static dispatch_queue_t queue = nil;
 
 @interface RNKitSensor ()
 
-@property (nonatomic, strong) NSMutableArray *modelArray;
-@property (nonatomic, strong) NSMutableArray *jsonArray;
-@property (nonatomic, assign) BOOL enterBackground;
-@property (nonatomic, assign) NSInteger maxUploadNum;//有网情况下,上传失败,最多重复上传三次
-
-@property (nonatomic, assign) int yy;
+@property (nonatomic, strong) Utils *utils;
 
 @end
 
@@ -43,260 +28,66 @@ static dispatch_queue_t queue = nil;
 
 RCT_EXPORT_MODULE();
 
-
-RCT_EXPORT_METHOD(initial:(NSString *)appkey maxVolume:(NSInteger)maxVolume)
+/**
+ * 初始化
+ *
+ * @param appKey      服务器分配的appKey
+ * @param maxVolume   一次最大上传的埋点条数
+ * @param repeatTimes 最大尝试次数
+ */
+RCT_EXPORT_METHOD(initial:(NSString *)appkey maxVolume:(NSInteger)maxVolume repeatTimes:(NSInteger)repeatTimes)
 {
-    [Utils setMaxVolume:maxVolume];
-    [Utils setAppkey:appkey];
-    NSLog(@"最大maxVolume:%ld,appkey:%@",(long)maxVolume,appkey);
-    [self setupNotifications];
-    RCTLogInfo(@"初始化");
+    [self.utils initial:appkey maxVolume:maxVolume repeatTimes:repeatTimes];
 }
 
+
+/**
+ * 存储到本地数据库
+ *
+ * @param jsonBody   埋点数据
+ * @param requestUrl 请求的Url
+ * @param level   这条埋点数据的优先级
+ */
 
 RCT_EXPORT_METHOD(save:(NSString *)jsonBody requestUrl:(NSString *)requestUrl priorityLevel:(NSInteger)level)
 {
-    
-    [[DataBaseHandle shareDataBase] openDB];
-    
-    NSDate *now = [NSDate date];
-    DataBaseModel *model = [DataBaseModel new];
-    model.jsonBody = [self isNullString:jsonBody] ? jsonBody : @"NO jsonBody";
-    model.requestUrl = [self isNullString:requestUrl] ? requestUrl : @"NO requestUrl";
-    model.priority = [self isNullString:@(level)] ? level : 20;
-    model.timeStamp = (NSUInteger)(([now timeIntervalSince1970]) * 1000);
-    model.times = 0;
-    model.status = 0;
-    [[DataBaseHandle shareDataBase] insertModel:model];
-    
-    [[DataBaseHandle shareDataBase] closeDB];
-    
+    [self.utils insertToDB:jsonBody requestUrl:requestUrl priorityLevel:level];
 }
 
 
+/**
+ * 检查本地是否有没有发送的埋点
+ */
 RCT_EXPORT_METHOD(check)
 {
-    [self upload];
-    RCTLogInfo(@"启动线程");
-    
-}
-
-- (void)upload {
-    
-    NSLog(@"最大maxVolume:%ld,appkey:%@",[Utils getMaxVolume],[Utils getAppkey]);
-    if (!([Utils getMaxVolume] && [self isNullString:[Utils getAppkey]])) {
-        NSLog(@"缺少上传最大量或者appkey值");
-        return;
-    }
-    
-    Reachability *reachAblilty = [Reachability reachabilityWithHostName:@"www.baidu.com"];
-    NetworkStatus status = [reachAblilty currentReachabilityStatus];
-    if (status == NotReachable){
-        NSLog(@"无网络");
-        return;
-    }
-    
-    //    [[DataBaseHandle shareDataBase] openDB];
-    //    [[DataBaseHandle shareDataBase] deleteWithStatus:1];
-    
-    if (queue == nil) {
-        queue = dispatch_queue_create("io.rnkit.sensor", DISPATCH_QUEUE_SERIAL);
-    }
-    
-    __weak typeof(self) weakSelf = self;
-    
-    dispatch_async(queue, ^{
-        NSLog(@"又开始了......");
-        
-        while (true) {
-            _yy++;
-            NSLog(@"yy的值:=====%d",_yy);
-            if (weakSelf.enterBackground || _yy > 10000) {
-                NSLog(@"停止了.....");
-                break;
-            }
-        }
-        /*
-         while (true) {
-         
-         if (weakSelf.enterBackground || weakSelf.maxUploadNum >= 3) {
-         break;
-         }
-         
-         
-         if (((NSArray *)[[DataBaseHandle shareDataBase] selectWithLimit:[Utils getMaxVolume]]).count > 0) {
-         
-         weakSelf.modelArray = [NSMutableArray array];
-         weakSelf.jsonArray = [NSMutableArray array];
-         
-         for (DataBaseModel *model in [[DataBaseHandle shareDataBase] selectWithLimit:[Utils getMaxVolume]]) {
-         model.times += 1;
-         [weakSelf.modelArray addObject:model];
-         [weakSelf.jsonArray addObject:model.jsonBody];
-         
-         }
-         
-         [weakSelf request];
-         
-         } else {
-         
-         [[DataBaseHandle shareDataBase] resetId];
-         
-         [[DataBaseHandle shareDataBase] closeDB];
-         
-         break;
-         }
-         }
-         */
-    });
-
+    [self.utils upload];
+   
 }
 
 
-- (BOOL)isNullString:(id)value {
-    NSString *str = [NSString stringWithFormat:@"%@",value];
-    if (str == nil || [str isEqual:[NSNull null]] || [str isEqualToString:@""]) {
-        return NO;
-    }
-    return YES;
-}
 
-
-- (NSString *)getMD5:(NSString *)string {
-    
-    const char *original_str = [string UTF8String];
-    unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(original_str,(int)strlen(original_str), result);//调用md5
-    NSMutableString *hash = [NSMutableString string];
-    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++){
-        [hash appendFormat:@"%02x", result[i]];
-    }
-    return hash;
-}
-
-- (void)request {
-    
-    NSDate *now = [NSDate date];
-    NSUInteger timeStamp = (NSUInteger)(([now timeIntervalSince1970]) * 1000);
-    
-    NSMutableString *jsonStr = [NSMutableString string];
-    for (NSString *jsonBody in self.jsonArray) {
-        [jsonStr appendFormat:@"%@,",jsonBody];
-    }
-    jsonStr = ([jsonStr substringToIndex:jsonStr.length - 1]).mutableCopy;
-    NSString *signatureString = [self getMD5:[NSString stringWithFormat:@"%@%@%lu",[Utils getAppkey],jsonStr,(unsigned long)timeStamp]];
-    
-    NSString *requestUrl = ((DataBaseModel *)self.modelArray[0]).requestUrl;
-    NSURL *url = [NSURL URLWithString:requestUrl];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    NSString *parmStr = [NSString stringWithFormat:@"iscompress=false&signature=%@&timestamp=%lu",signatureString,(unsigned long)timeStamp];
-    
-    request.HTTPBody = [parmStr dataUsingEncoding:NSUTF8StringEncoding];
-    request.HTTPMethod = @"POST";
-    
-    if (kIOS9) {
-        NSURLSession *session = [NSURLSession sharedSession];
-        NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-            [self responseData:data error:error];
-        }];
-        [task resume];
+/**
+ * 达到失败次数上限删除数量
+ */
+RCT_REMAP_METHOD(getFailCount,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    int count = [[[NSUserDefaults standardUserDefaults] objectForKey:@"fail_repeatCount"] intValue];
+    RCTLogInfo(@"最大上传次数失败次数====%d",count);
+    if (count >= 0) {
+        resolve(count);
     } else {
-        [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
-            [self responseData:data error:connectionError];
-        }];
-        
-    }
-    
-    if (self.modelArray.count > 0) {
-        [[DataBaseHandle shareDataBase] batchUpdeate:self.modelArray];
-        [[DataBaseHandle shareDataBase] deleteWithStatus:1];
-    }
-
-}
-
-
-- (void)responseData:(NSData *)data error:(NSError *)error {
-    
-    if (error) {
-        [self changeStatus:2];
-        self.maxUploadNum += 1;
-    } else {
-        
-        NSDictionary *dict = [self toArrayOrNSDictionary:data];
-        if (dict) {
-            if ([dict[@"code"] intValue] == 1) {
-                [self changeStatus:1];
-                self.maxUploadNum = 0;
-            } else {
-                [self changeStatus:2];
-                self.maxUploadNum += 1;
-            }
-        }else{
-            [self changeStatus:2];
-            self.maxUploadNum += 1;
-        }
-    }
-
-}
-
-
-
-
-- (void)changeStatus:(NSInteger)status {
-    
-    for (DataBaseModel *model in self.modelArray) {
-        @autoreleasepool {
-            model.status = status;
-        }
+        reject(@"无超过最大上传次数的数据");
     }
 }
 
 
-
-- (id)toArrayOrNSDictionary:(NSData *)jsonData{
-    
-    NSError *error = nil;
-    
-    id jsonObject = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:&error];
-    
-    if (jsonObject != nil && error == nil) {
-        
-        return jsonObject;
-        
-    } else {
-        
-        return nil;
-        
+- (Utils *)utils {
+    if (!_utils) {
+        _utils = [Utils new];
     }
-    
-}
-
-
-- (void)setupNotifications
-{
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationWillEnterForeground:) name:UIApplicationWillEnterForegroundNotification object:nil];
-}
-
-
-#pragma mark Background tasks
-
-- (void)applicationDidEnterBackground:(NSNotification *)notification
-{
-    self.enterBackground = YES;
-    NSLog(@"进入后台了");
-}
-
-- (void)applicationWillEnterForeground:(NSNotification *)notification
-{
-    self.enterBackground = NO;
-    [self upload];
-  
-}
-
-- (void)dealloc
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    return _utils;
 }
 
 
